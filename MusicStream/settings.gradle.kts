f3cee13a9dbbd0ab372453126115e1d420e5886b
nell-seeen <cristiandwih2006@gmail.com>
2026-06-10 15:15:58 +0800

    Update settings.gradle.kts

--- MusicStream/settings.gradle.kts
+++ MusicStream/settings.gradle.kts
@@ -11,6 +11,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+// TRIK SAKTI: Memaksa Gradle mendownload SDK sendiri jika tidak ditemukan di Codespaces
+providers.gradleProperty("android.sdk.elg").orNull?.let {
+    System.setProperty("android.builder.sdkDownload", "true")
+}
+System.setProperty("android.builder.sdkDownload", "true")
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
